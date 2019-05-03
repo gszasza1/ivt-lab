ffeeab33e0c3b13e0328b3bd9a3a819668f038a9
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2019-05-03 02:06:01 -0700

    REp

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -38,7 +38,7 @@ public class TorpedoStore {
 
     // simulate random overheating of the launcher bay which prevents firing
 
-    double r = generator.nextDouble();
+    double r = this.generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
